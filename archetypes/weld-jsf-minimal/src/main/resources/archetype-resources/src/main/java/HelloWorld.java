13475fb094a747dc3bf7ccb624f2b69f720ae03c
Dan Allen <dan.j.allen@gmail.com>
2009-11-13 19:30:41 +0000

    add bean validation in the example class and view

--- archetypes/weld-jsf-minimal/src/main/resources/archetype-resources/src/main/java/HelloWorld.java
+++ archetypes/weld-jsf-minimal/src/main/resources/archetype-resources/src/main/java/HelloWorld.java
@@ -1,24 +1,70 @@
 package ${package};
 
-import java.io.Serializable;
+import javax.enterprise.inject.Model;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
-@RequestScoped
-@Named
-public class HelloWorld implements Serializable
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public @Model class HelloWorld
 {
+   private final String text = "Hello World!";
+
+   private String letters;
+   
+   private String numbers;
+   
+   private String email;
+   
    public HelloWorld()
    {
       System.out.println(this.getClass().getSimpleName() + " was constructed");
    }
-
-   private final String text = "Hello World!";
 
    public String getText()
    {
       return text;
    }
 
-   private static final long serialVersionUID = 1L;
+   @NotNull
+   @NotEmpty
+   @Pattern(regexp = "[A-Za-z]*", message = "must contain only letters")
+   public String getLetters()
+   {
+      return letters;
+   }
+
+   public void setLetters(String letters)
+   {
+      this.letters = letters;
+   }
+
+   @NotNull
+   @NotEmpty
+   @Digits(fraction = 0, integer = 2)
+   public String getNumbers()
+   {
+      return numbers;
+   }
+
+   public void setNumbers(String numbers)
+   {
+      this.numbers = numbers;
+   }
+
+   @NotNull
+   @NotEmpty
+   @Email
+   public String getEmail()
+   {
+      return email;
+   }
+
+   public void setEmail(String email)
+   {
+      this.email = email;
+   }
+
 }
