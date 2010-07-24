6c139d03b2fb0da40a984b93d0f84ddd3ae32015
Pete Muir <pmuir@bleepbleep.org.uk>
2010-07-24 12:15:41 +0000

    Remove AutoProxy for now, we can reconstruct from SVN history

--- impl/src/test/java/org/jboss/weld/extensions/test/autoproxy/HelloGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.weld.extensions.test.autoproxy;
-
-public class HelloGenerator
-{
-   public String getHelloString()
-   {
-      return "Hello ";
-   }
-}
--- impl/src/test/java/org/jboss/weld/extensions/test/autoproxy/HelloService.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.weld.extensions.test.autoproxy;
-
-import org.jboss.weld.extensions.autoproxy.AutoProxy;
-
-@AutoProxy(HelloServiceHandler.class)
-public interface HelloService
-{
-
-}
