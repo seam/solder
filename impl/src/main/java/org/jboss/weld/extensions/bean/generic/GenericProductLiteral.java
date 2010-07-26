010f4e2560a109a6523879c57e33248709fb58e8
Pete Muir <pmuir@bleepbleep.org.uk>
2010-07-26 16:45:22 +0100

    fix compilation problems

---
+++ impl/src/main/java/org/jboss/weld/extensions/bean/generic/GenericProductLiteral.java
@@ -0,0 +1,15 @@
+package org.jboss.weld.extensions.bean.generic;
+
+import javax.enterprise.util.AnnotationLiteral;
+
+
+public class GenericProductLiteral extends AnnotationLiteral<GenericProduct> implements GenericProduct 
+{
+   
+   public static final GenericProduct INSTANCE = new GenericProductLiteral();
+
+   private static final long serialVersionUID = 2768505716290514234L;
+   
+   public GenericProductLiteral() {}
+
+}
