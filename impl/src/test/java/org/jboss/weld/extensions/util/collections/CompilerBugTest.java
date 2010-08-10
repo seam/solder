7a0c72c76dd642329e047ff641cbb5798a357d27
Stuart Douglas <stuart.w.douglas@gmail.com>
2010-08-10 20:21:55 +0800

    test that demonstrates the compiler bug

---
+++ impl/src/test/java/org/jboss/weld/extensions/util/collections/CompilerBugTest.java
@@ -0,0 +1,18 @@
+package org.jboss.weld.extensions.util.collections;
+
+import org.junit.Test;
+
+/**
+ * tests for the presenece of a specific compiler bug
+ * 
+ * @author stuart
+ * 
+ */
+public class CompilerBugTest
+{
+   @Test
+   public void testcompilerBug()
+   {
+      AbstractMultimap.WrappedList.WrappedListIterator.class.getTypeParameters();
+   }
+}
