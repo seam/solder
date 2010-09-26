003d1f2d19767182b08e1ca968f111ea00161114
Pete Muir <pmuir@bleepbleep.org.uk>
2010-09-26 13:20:28 +0100

    Add package-info.java for reflections

---
+++ impl/src/main/java/org/jboss/weld/extensions/reflection/annotated/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>
+ * A builder class for creating {@link javax.enterprise.inject.spi.AnnotatedType} instances
+ * from an existing {@link javax.enterprise.inject.spi.AnnotatedType} or a {@link java.lang.Class}.
+ * </p>
+ * 
+ * @author Pete Muir
+ * 
+ * @see org.jboss.weld.extensions.reflection.annotated.AnnotatedTypeBuilder
+ */
+package org.jboss.weld.extensions.reflection.annotated;
