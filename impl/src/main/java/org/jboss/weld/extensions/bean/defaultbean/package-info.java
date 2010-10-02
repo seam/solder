24c694f221d05e00e4d493b898e250466f53871f
Pete Muir <pmuir@bleepbleep.org.uk>
2010-10-02 17:20:12 +0100

    Docs for default beans
    * Add package-info
    * Add formatting to javadoc
    * Add producers to reference docs

---
+++ impl/src/main/java/org/jboss/weld/extensions/bean/defaultbean/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * <p>
+ * Allows a default implementation of a particular service to be provided and overridden as needed. Although this may 
+ * sound identical to an alternative, alternatives have some restrictions that may make them undesirable. Primarily, 
+ * alternatives require an entry in every <code>beans.xml</code> file in an application.
+ * </p>
+ * 
+ * <p>
+ * Developers consuming the extension will have to open up the any jar file which references the default bean, and edit 
+ * the <code>beans.xml</code> file within, in order to override the service. This is where default beans come in.
+ * </p>
+ * 
+ * <p>
+ * Default beans allow you to create a default bean with a specified type and set of qualifiers. If no other bean is
+ * installed that has the same type and qualifiers, then the default bean will be installed.
+ * </p>
+ * 
+ * @see org.jboss.weld.extensions.bean.defaultbean.DefaultBean
+ */
+package org.jboss.weld.extensions.bean.defaultbean;
+
