e17b51f958d362f25768fe74c1aaa020ec7fa084
Stuart Douglas <stuart.w.douglas@gmail.com>
2010-10-04 21:15:07 +1100

    Add javadoc to Annotateds

--- impl/src/main/java/org/jboss/weld/extensions/reflection/annotated/Annotateds.java
+++ impl/src/main/java/org/jboss/weld/extensions/reflection/annotated/Annotateds.java
@@ -45,8 +45,8 @@ import javax.enterprise.inject.spi.AnnotatedType;
  * </p>
  * 
  * <p>
- * Includes utilities to check the equality of {@link Annotated} instances, and
- * to create unique ids for {@link Annotated} instances.
+ * Includes utilities to check the equality of and create unique id's for
+ * <code>Annotated</code> instances.
  * </p>
  * 
  * @author Stuart Douglas <stuart@baileyroberts.com.au>
@@ -190,12 +190,15 @@ public class Annotateds
    private Annotateds() {}
 
    /**
-    * Generates a unique signature for an annotated type. Members without
-    * annotations are omitted to reduce the length of the signature
+    * Generates a deterministic signature for an {@link AnnotatedType}. Two
+    * <code>AnnotatedType</code>s that have the same annotations and underlying
+    * type will generate the same signature.
+    * <p/>
+    * This can be used to create a unique bean id for a passivation capable bean
+    * that is added directly through the SPI.
     * 
-    * @param <X>
-    * @param annotatedType
-    * @return
+    * @param annotatedType The type to generate a signature for
+    * @return A string representation of the annotated type
     */
    public static <X> String createTypeId(AnnotatedType<X> annotatedType)
    {
@@ -203,11 +206,17 @@ public class Annotateds
    }
 
    /**
-    * Generates a unique signature for a concrete class
+    * Generates a unique signature for a concrete class. Annotations are not
+    * read directly from the class, but are read from the
+    * <code>annotations</code>, <code>methods</code>, <code>fields</code> and
+    * <code>constructors</code> arguments
     * 
-    * @param <X>
-    * @param annotatedType
-    * @return
+    * @param clazz The java class tyoe
+    * @param annotations Annotations present on the java class
+    * @param methods The AnnotatedMethods to include in the signature
+    * @param fields The AnnotatedFields to include in the signature
+    * @param constructors The AnnotatedConstructors to include in the signature
+    * @return A string representation of the type
     */
    public static <X> String createTypeId(Class<X> clazz, Collection<Annotation> annotations, Collection<AnnotatedMethod<? super X>> methods, Collection<AnnotatedField<? super X>> fields, Collection<AnnotatedConstructor<X>> constructors)
    {
@@ -260,87 +269,22 @@ public class Annotateds
       return builder.toString();
    }
 
-   private static <X> boolean hasMethodParameters(AnnotatedCallable<X> callable)
-   {
-      for (AnnotatedParameter<X> parameter : callable.getParameters())
-      {
-         if (!parameter.getAnnotations().isEmpty())
-         {
-            return true;
-         }
-      }
-      return false;
-   }
-
-   private static String createAnnotationCollectionId(Collection<Annotation> annotations)
-   {
-      if (annotations.isEmpty())
-      {
-         return "";
-      }
-
-      StringBuilder builder = new StringBuilder();
-      builder.append('[');
-
-      List<Annotation> annotationList = new ArrayList<Annotation>(annotations.size());
-      annotationList.addAll(annotations);
-      Collections.sort(annotationList, AnnotationComparator.INSTANCE);
-
-      for (Annotation a : annotationList)
-      {
-         builder.append('@');
-         builder.append(a.annotationType().getName());
-         builder.append('(');
-         Method[] declaredMethods = a.annotationType().getDeclaredMethods();
-         List<Method> methods = new ArrayList<Method>(declaredMethods.length);
-         for (Method m : declaredMethods)
-         {
-            methods.add(m);
-         }
-         Collections.sort(methods, MethodComparator.INSTANCE);
-
-         for (int i = 0; i < methods.size(); ++i)
-         {
-            Method method = methods.get(i);
-            try
-            {
-               Object value = method.invoke(a);
-               builder.append(method.getName());
-               builder.append('=');
-               builder.append(value.toString());
-            }
-            catch (NullPointerException e)
-            {
-               throw new RuntimeException("NullPointerException accessing annotation member, annotation:" + a.annotationType().getName() + " member: " + method.getName(), e);
-            }
-            catch (IllegalArgumentException e)
-            {
-               throw new RuntimeException("IllegalArgumentException accessing annotation member, annotation:" + a.annotationType().getName() + " member: " + method.getName(), e);
-            }
-            catch (IllegalAccessException e)
-            {
-               throw new RuntimeException("IllegalAccessException accessing annotation member, annotation:" + a.annotationType().getName() + " member: " + method.getName(), e);
-            }
-            catch (InvocationTargetException e)
-            {
-               throw new RuntimeException("InvocationTargetException accessing annotation member, annotation:" + a.annotationType().getName() + " member: " + method.getName(), e);
-            }
-            if (i + 1 != methods.size())
-            {
-               builder.append(',');
-            }
-         }
-         builder.append(')');
-      }
-      builder.append(']');
-      return builder.toString();
-   }
-
+   /**
+    * Generates a deterministic signature for an {@link AnnotatedField}. Two
+    * <code>AnnotatedField</code>s that have the same annotations and
+    * underlying field will generate the same signature.
+    */
    public static <X> String createFieldId(AnnotatedField<X> field)
    {
       return createFieldId(field.getJavaMember(), field.getAnnotations());
    }
 
+   /**
+    * Creates a deterministic signature for a {@link Field}.
+    * 
+    * @param field The field to generate the signature for
+    * @param annotations The annotations to include in the signature
+    */
    public static <X> String createFieldId(Field field, Collection<Annotation> annotations)
    {
       StringBuilder builder = new StringBuilder();
@@ -351,6 +295,11 @@ public class Annotateds
       return builder.toString();
    }
 
+   /**
+    * Generates a deterministic signature for an {@link AnnotatedCallable}. Two
+    * <code>AnnotatedCallable</code>s that have the same annotations and
+    * underlying callable will generate the same signature.
+    */
    public static <X> String createCallableId(AnnotatedCallable<X> method)
    {
       StringBuilder builder = new StringBuilder();
@@ -362,6 +311,14 @@ public class Annotateds
       return builder.toString();
    }
 
+   /**
+    * Creates a deterministic signature for a {@link Method}.
+    * 
+    * @param method The method to generate the signature for
+    * @param annotations The annotations to include in the signature
+    * @param parameters The {@link AnnotatedParameter}s to include in the
+    *           signature
+    */
    public static <X> String createMethodId(Method method, Set<Annotation> annotations, List<AnnotatedParameter<X>> parameters)
    {
       StringBuilder builder = new StringBuilder();
@@ -373,6 +330,14 @@ public class Annotateds
       return builder.toString();
    }
 
+   /**
+    * Creates a deterministic signature for a {@link Constructor}.
+    * 
+    * @param constructor The constructor to generate the signature for
+    * @param annotations The annotations to include in the signature
+    * @param parameters The {@link AnnotatedParameter}s to include in the
+    *           signature
+    */
    public static <X> String createConstructorId(Constructor<X> constructor, Set<Annotation> annotations, List<AnnotatedParameter<X>> parameters)
    {
       StringBuilder builder = new StringBuilder();
@@ -384,6 +349,10 @@ public class Annotateds
       return builder.toString();
    }
 
+   /**
+    * Generates a unique string representation of a list of
+    * {@link AnnotatedParameter}s.
+    */
    public static <X> String createParameterListId(List<AnnotatedParameter<X>> parameters)
    {
       StringBuilder builder = new StringBuilder();
@@ -401,11 +370,18 @@ public class Annotateds
       return builder.toString();
    }
 
+   /**
+    * Creates a string representation of an {@link AnnotatedParameter}.
+    */
    public static <X> String createParameterId(AnnotatedParameter<X> annotatedParameter)
    {
       return createParameterId(annotatedParameter.getBaseType(), annotatedParameter.getAnnotations());
    }
 
+   /**
+    * Creates a string representation of a given type and set of annotations.
+    * 
+    */
    public static <X> String createParameterId(Type type, Set<Annotation> annotations)
    {
       StringBuilder builder = new StringBuilder();
@@ -423,37 +399,14 @@ public class Annotateds
    }
 
    /**
-    * compares two annotated elemetes to see if they have the same annotations
-    * 
-    * @param a1
-    * @param a2
-    * @return
+    * <p>
+    * Compares {@link AnnotatedField}s for equality.
+    * </p>
+    * <p>
+    * Two {@link AnnotatedField}s are considered equal if they have the same
+    * underlying field and annotations.
+    * </p>
     */
-   private static boolean compareAnnotated(Annotated a1, Annotated a2)
-   {
-      return a1.getAnnotations().equals(a2.getAnnotations());
-   }
-
-   /**
-    * compares two annotated elements to see if they have the same annotations
-    * 
-    */
-   private static boolean compareAnnotatedParameters(List<? extends AnnotatedParameter<?>> p1, List<? extends AnnotatedParameter<?>> p2)
-   {
-      if (p1.size() != p2.size())
-      {
-         return false;
-      }
-      for (int i = 0; i < p1.size(); ++i)
-      {
-         if (!compareAnnotated(p1.get(i), p2.get(i)))
-         {
-            return false;
-         }
-      }
-      return true;
-   }
-
    public static boolean compareAnnotatedField(AnnotatedField<?> f1, AnnotatedField<?> f2)
    {
       if (!f1.getJavaMember().equals(f2.getJavaMember()))
@@ -463,6 +416,16 @@ public class Annotateds
       return compareAnnotated(f1, f2);
    }
 
+   /**
+    * <p>
+    * Compare {@link AnnotatedCallable}s for equality.
+    * </p>
+    * 
+    * <p>
+    * Two {@link AnnotatedCallable}s are considered equal if they have the same
+    * underlying callable and annotations.
+    * </p>
+    */
    public static boolean compareAnnotatedCallable(AnnotatedCallable<?> m1, AnnotatedCallable<?> m2)
    {
       if (!m1.getJavaMember().equals(m2.getJavaMember()))
@@ -477,7 +440,15 @@ public class Annotateds
    }
 
    /**
-    * Compares two annotated types and returns true if they are the same
+    * <p>
+    * Compares two {@link AnnotatedType}s for equality.
+    * </p>
+    * 
+    * <p>
+    * Two {@link AnnotatedType}s are considered equal if they have the same
+    * underlying type and annotations, and all members have the same
+    * annotations.
+    * </p>
     */
    public static boolean compareAnnotatedTypes(AnnotatedType<?> t1, AnnotatedType<?> t2)
    {
@@ -562,6 +533,111 @@ public class Annotateds
       }
       return true;
 
+   }
+
+   private static <X> boolean hasMethodParameters(AnnotatedCallable<X> callable)
+   {
+      for (AnnotatedParameter<X> parameter : callable.getParameters())
+      {
+         if (!parameter.getAnnotations().isEmpty())
+         {
+            return true;
+         }
+      }
+      return false;
+   }
+
+   private static String createAnnotationCollectionId(Collection<Annotation> annotations)
+   {
+      if (annotations.isEmpty())
+      {
+         return "";
+      }
+
+      StringBuilder builder = new StringBuilder();
+      builder.append('[');
+
+      List<Annotation> annotationList = new ArrayList<Annotation>(annotations.size());
+      annotationList.addAll(annotations);
+      Collections.sort(annotationList, AnnotationComparator.INSTANCE);
+
+      for (Annotation a : annotationList)
+      {
+         builder.append('@');
+         builder.append(a.annotationType().getName());
+         builder.append('(');
+         Method[] declaredMethods = a.annotationType().getDeclaredMethods();
+         List<Method> methods = new ArrayList<Method>(declaredMethods.length);
+         for (Method m : declaredMethods)
+         {
+            methods.add(m);
+         }
+         Collections.sort(methods, MethodComparator.INSTANCE);
+
+         for (int i = 0; i < methods.size(); ++i)
+         {
+            Method method = methods.get(i);
+            try
+            {
+               Object value = method.invoke(a);
+               builder.append(method.getName());
+               builder.append('=');
+               builder.append(value.toString());
+            }
+            catch (NullPointerException e)
+            {
+               throw new RuntimeException("NullPointerException accessing annotation member, annotation:" + a.annotationType().getName() + " member: " + method.getName(), e);
+            }
+            catch (IllegalArgumentException e)
+            {
+               throw new RuntimeException("IllegalArgumentException accessing annotation member, annotation:" + a.annotationType().getName() + " member: " + method.getName(), e);
+            }
+            catch (IllegalAccessException e)
+            {
+               throw new RuntimeException("IllegalAccessException accessing annotation member, annotation:" + a.annotationType().getName() + " member: " + method.getName(), e);
+            }
+            catch (InvocationTargetException e)
+            {
+               throw new RuntimeException("InvocationTargetException accessing annotation member, annotation:" + a.annotationType().getName() + " member: " + method.getName(), e);
+            }
+            if (i + 1 != methods.size())
+            {
+               builder.append(',');
+            }
+         }
+         builder.append(')');
+      }
+      builder.append(']');
+      return builder.toString();
+   }
+
+   /**
+    * Compares two annotated elements to see if they have the same annotations
+    * 
+    */
+   private static boolean compareAnnotated(Annotated a1, Annotated a2)
+   {
+      return a1.getAnnotations().equals(a2.getAnnotations());
+   }
+
+   /**
+    * Compares two annotated elements to see if they have the same annotations
+    * 
+    */
+   private static boolean compareAnnotatedParameters(List<? extends AnnotatedParameter<?>> p1, List<? extends AnnotatedParameter<?>> p2)
+   {
+      if (p1.size() != p2.size())
+      {
+         return false;
+      }
+      for (int i = 0; i < p1.size(); ++i)
+      {
+         if (!compareAnnotated(p1.get(i), p2.get(i)))
+         {
+            return false;
+         }
+      }
+      return true;
    }
 
 }
